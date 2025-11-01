fca286df154f6c99769bc300654d9a24c3b6f346
Andeglieri <guilherme.andeglieri@penseavanti.com.br>
2025-10-31 23:51:54 -0300

    endpoint fix

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,7 @@ import { login, register } from '../controllers/auth'
 
 const router = express.Router();
 
-router.post("/", login)
-router.post("/", register)
+router.post("/login", login)
+router.post("/register", register)
 
 export default router
